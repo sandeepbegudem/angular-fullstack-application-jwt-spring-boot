0040e35259401f33bf7d48db0f911954a234779e
Sandeep B <124546985+sandeepbegudem@users.noreply.github.com>
2024-05-14 22:36:41 -0500

    Update students.component.ts

--- angular-frontend/src/app/components/students/students.component.ts
+++ angular-frontend/src/app/components/students/students.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { catchError, Observable, throwError } from 'rxjs';
+import { PageResponse } from 'src/app/model/page.response.model';
+import { StudentDTO } from 'src/app/model/student.model';
 import { InstructorsService } from 'src/app/services/instructors.service';
+import { StudentsService } from 'src/app/services/students.service';
 
 @Component({
   selector: 'app-students',
@@ -12,12 +16,25 @@ import { InstructorsService } from 'src/app/services/instructors.service';
 
 export class StudentsComponent implements OnInit {
 
+  searchFormGroup!: FormGroup;
+  pageStudents!: Observable<PageResponse<StudentDTO>>
+
+  errorMessage!: string;
+  currentPage : number = 0;
+  pageSize : number = 5;
+
   constructor(
     private modalService: NgbModal,
-    private fb: FormBuilder, private instructorService: InstructorsService
+    private fb: FormBuilder,
+    private instructorService: InstructorsService,
+    private studentService : StudentsService
     ) {}
 
   ngOnInit(): void {
+    this.searchFormGroup = this.fb.group({
+      keyword: this.fb.control('')
+    })
+    this.handleSearchStudents();
   }
 
 
@@ -26,5 +43,19 @@ export class StudentsComponent implements OnInit {
     console.log("Hello world")
   }
 
+  handleSearchStudents() {
+    let keyword = this.searchFormGroup.value.keyword
+    this.pageStudents = this.studentService.searchStudents(keyword, this.currentPage, this.pageSize).pipe(
+      catchError(err => {
+        this.errorMessage = err.message;
+        return throwError(err)
+      })
+    )
+  }
+
+  gotoPage(page : number) {
+    this.currentPage = page;
+    this.handleSearchStudents();
+  }
 
 }
