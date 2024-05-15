9aa82bcc38bdb98455d26d55c274b303c83d052f
Sandeep B <124546985+sandeepbegudem@users.noreply.github.com>
2024-05-14 22:38:55 -0500

    Add Student Service ts files

---
+++ angular-frontend/src/app/services/students.service.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+
+import { StudentsService } from './students.service';
+
+
+describe('StudentsService', () => {
+  let service: StudentsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(StudentsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy(); 
+  }); 
+});
+ 
---
+++ angular-frontend/src/app/services/students.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { PageResponse } from '../model/page.response.model';
+import { StudentDTO } from '../model/student.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentsService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public searchStudents(keyword : string, currentPage: number, pageSize: number) : Observable<PageResponse<StudentDTO>> {
+    return this.httpClient.get<PageResponse<StudentDTO>>(environment.backendHost + "/students?keyword=" + keyword + "&currentPage=" + currentPage + "&pageSize=" + pageSize);
+  } 
+}
