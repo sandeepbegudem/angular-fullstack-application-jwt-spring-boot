46670f5f4eef0b9116f452d19e23e5dc3daa6b40
Sandeep B <124546985+sandeepbegudem@users.noreply.github.com>
2024-05-15 19:29:50 -0500

    Update student.model.ts

--- angular-frontend/src/app/model/student.model.ts
+++ angular-frontend/src/app/model/student.model.ts
@@ -1,6 +1,9 @@
+import { User } from "./user.model";
+
 export interface StudentDTO {
     studentId: number;
     firstName: string;
     lastName: string;
-    level: string; 
+    level: string;
+    user: User;
 }
