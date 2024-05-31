288cab115529c957c78c35187283c689783fdc49
Sandeep B <124546985+sandeepbegudem@users.noreply.github.com>
2024-05-31 12:39:29 -0500

    Update JWTAuthenticationFilter.java

--- backend.application/src/main/java/com/sandeepbegudem/backend/application/filter/JWTAuthenticationFilter.java
+++ backend.application/src/main/java/com/sandeepbegudem/backend/application/filter/JWTAuthenticationFilter.java
@@ -1,1 +1,51 @@
-package com.sandeepbegudem.backend.application.filter;import com.fasterxml.jackson.databind.ObjectMapper;import com.sandeepbegudem.backend.application.helper.JWTHelper;import jakarta.servlet.FilterChain;import jakarta.servlet.http.HttpServletRequest;import jakarta.servlet.http.HttpServletResponse;import org.springframework.security.authentication.AuthenticationManager;import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;import org.springframework.security.core.Authentication;import org.springframework.security.core.AuthenticationException;import org.springframework.security.core.GrantedAuthority;import org.springframework.security.core.userdetails.User;import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;import java.io.IOException;import java.util.stream.Collectors;public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {    private final AuthenticationManager authenticationManager;    private final JWTHelper jwtHelper;    public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTHelper jwtHelper) {        this.authenticationManager = authenticationManager;        this.jwtHelper = jwtHelper;    }    @Override    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {        String email = request.getParameter("username");        String password = request.getParameter("password");        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);        return authenticationManager.authenticate(authenticationToken);    }    @Override    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {        User user = (User) authResult.getPrincipal();        String jwtAccessToken = jwtHelper.generateAccessToken(user.getUsername(),                user.getAuthorities().stream()                        .map(GrantedAuthority::getAuthority)                        .collect(Collectors.toList()));        String jwtRefreshToken = jwtHelper.generateRefreshToken(user.getUsername());        response.setContentType("application/json");        new ObjectMapper().writeValue(response.getOutputStream(), jwtHelper.getTokensMap(jwtAccessToken, jwtRefreshToken));    }}
+package com.sandeepbegudem.backend.application.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sandeepbegudem.backend.application.helper.JWTHelper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JWTHelper jwtHelper;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTHelper jwtHelper) {
+        this.authenticationManager = authenticationManager;
+        this.jwtHelper = jwtHelper;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String email = request.getParameter("username");
+        String password = request.getParameter("password");
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
+        User user = (User) authResult.getPrincipal();
+
+        String jwtAccessToken = jwtHelper.generateAccessToken(user.getUsername(),
+                user.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()));
+
+        String jwtRefreshToken = jwtHelper.generateRefreshToken(user.getUsername());
+        response.setContentType("application/json");
+        new ObjectMapper().writeValue(response.getOutputStream(), jwtHelper.getTokensMap(jwtAccessToken, jwtRefreshToken));
+
+    }
+}
