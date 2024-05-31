0c139880b1454fdd8731c6d796c92f89f31e5018
Sandeep B <124546985+sandeepbegudem@users.noreply.github.com>
2024-05-30 22:15:30 -0500

    feature: add PreAuthorize method level security to UserController.java

--- backend.application/src/main/java/com/sandeepbegudem/backend/application/controller/UserController.java
+++ backend.application/src/main/java/com/sandeepbegudem/backend/application/controller/UserController.java
@@ -1,1 +1,21 @@
-package com.sandeepbegudem.backend.application.controller;import com.sandeepbegudem.backend.application.repository.UserRepository;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.web.bind.annotation.*;@RestController@CrossOrigin("*")@RequestMapping("/api/v1/users")public class UserController {    @Autowired    private UserRepository userRepository;    @GetMapping    public boolean checkIfEmailExists(@RequestParam(name = "email", defaultValue = "") String email)  {        return userRepository.findByEmail(email) != null;    }}
+package com.sandeepbegudem.backend.application.controller;
+
+import com.sandeepbegudem.backend.application.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('Admin')")
+    public boolean checkIfEmailExists(@RequestParam(name = "email", defaultValue = "") String email)  {
+        return userRepository.findByEmail(email) != null;
+    }
+}
