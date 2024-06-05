71c260dae167e014ed3923a833fb29c2ae1df66c
Sandeep B <124546985+sandeepbegudem@users.noreply.github.com>
2024-06-05 18:26:00 -0500

    chore: update AUTH_WHITE_LIST and CORS Configuration

--- backend.application/src/main/java/com/sandeepbegudem/backend/application/config/SecurityConfiguration.java
+++ backend.application/src/main/java/com/sandeepbegudem/backend/application/config/SecurityConfiguration.java
@@ -1,1 +1,92 @@
-package com.sandeepbegudem.backend.application.config;import com.sandeepbegudem.backend.application.filter.JWTAuthenticationFilter;import com.sandeepbegudem.backend.application.filter.JWTAuthorizationFilter;import com.sandeepbegudem.backend.application.helper.JWTHelper;import org.springframework.context.annotation.Bean;import org.springframework.context.annotation.Configuration;import org.springframework.security.authentication.AuthenticationManager;import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;import org.springframework.security.config.annotation.web.builders.HttpSecurity;import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;import org.springframework.security.config.http.SessionCreationPolicy;import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;import org.springframework.security.crypto.password.NoOpPasswordEncoder;import org.springframework.security.crypto.password.PasswordEncoder;import org.springframework.security.web.SecurityFilterChain;import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;@Configuration@EnableWebSecuritypublic class SecurityConfiguration {    private static final String[] AUTH_WHITE_LIST = {            "/login",            "/v3/api-docs/**",            "/swagger-ui/**",            "/v2/api-docs/**",            "/swagger-resources/**",            "/error"    };    private final JWTHelper jwtHelper;    public SecurityConfiguration(JWTHelper jwtHelper) {        this.jwtHelper = jwtHelper;    }    @SuppressWarnings("deprecation")    @Bean    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {        httpSecurity.cors(AbstractHttpConfigurer::disable)                .csrf(AbstractHttpConfigurer::disable)                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));        httpSecurity.authorizeRequests().requestMatchers(AUTH_WHITE_LIST).permitAll();        httpSecurity.authorizeRequests().requestMatchers("/api/v1/courses/**","/api/v1/students/**", "/api/v1/instructors/**").authenticated();        //httpSecurity.authorizeRequests().anyRequest().authenticated();        httpSecurity.addFilter(new JWTAuthenticationFilter(authenticationManager(httpSecurity.getSharedObject(AuthenticationConfiguration.class)), jwtHelper));        httpSecurity.addFilterBefore(new JWTAuthorizationFilter(jwtHelper), UsernamePasswordAuthenticationFilter.class);        return httpSecurity.build();    }    @Bean    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {        return authConfig.getAuthenticationManager();    }    @Bean    public PasswordEncoder passwordEncoder() {       // return NoOpPasswordEncoder.getInstance();        return new BCryptPasswordEncoder();    }}
+package com.sandeepbegudem.backend.application.config;
+
+import com.sandeepbegudem.backend.application.filter.JWTAuthenticationFilter;
+import com.sandeepbegudem.backend.application.filter.JWTAuthorizationFilter;
+import com.sandeepbegudem.backend.application.helper.JWTHelper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static java.util.Arrays.asList;
+import static java.util.List.*;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+
+    private static final String[] AUTH_WHITE_LIST = {
+            "/login",
+            "/refresh-token",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/v2/api-docs/**",
+            "/swagger-resources/**",
+            "/error"
+    };
+
+    private final JWTHelper jwtHelper;
+    public SecurityConfiguration(JWTHelper jwtHelper) {
+        this.jwtHelper = jwtHelper;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.authorizeRequests().requestMatchers(AUTH_WHITE_LIST).permitAll();
+        httpSecurity.authorizeRequests().requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+        httpSecurity.authorizeRequests().requestMatchers("/api/v1/courses/**","/api/v1/students/**", "/api/v1/instructors/**").authenticated();
+        //httpSecurity.authorizeRequests().anyRequest().authenticated();
+        httpSecurity.addFilter(new JWTAuthenticationFilter(authenticationManager(httpSecurity.getSharedObject(AuthenticationConfiguration.class)), jwtHelper));
+        httpSecurity.addFilterBefore(new JWTAuthorizationFilter(jwtHelper), UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+       // return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(of("http://localhost:4200"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        configuration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept","Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        configuration.setAllowedMethods(asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
